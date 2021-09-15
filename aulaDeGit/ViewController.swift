8fc5a40e5256c5ff28c8b6f02c3640f7800922b2
João Victor Ferreira Pimenta <joaovictorfp30@gmail.com>
2021-09-15 13:55:48 -0300

    Initial Commit

---
+++ aulaDeGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aulaDeGit
+//
+//  Created by João Victor Ferreira Pimenta on 15/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
